8e7a5c0d98b48acc50117dc9699f6784d0722495
banxi1988 <banxi1988@gmail.com>
2015-05-31 09:46:21 +0800

    first commit

---
+++ app/src/androidTest/java/com/banxi1988/v2ex_geek/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.banxi1988.v2ex_geek;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
